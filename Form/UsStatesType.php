a6f0cd3332232a9c05b9f3ac0219cc7d17b2a794
Napas <napas@napas.lt>
2014-06-25 12:26:52 +0100

    Added composer.json and US States form type

---
+++ Form/UsStatesType.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Napas\FormExtraBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * US States form field type
+ * @package Napas\FormExtraBundle\Form
+ */
+class UsStatesType extends AbstractType
+{
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'us-states';
+    }
+
+    /**
+     * @return null|string|\Symfony\Component\Form\FormTypeInterface
+     */
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'choices' => array(
+                'AL' => 'Alabama',
+                'AK' => 'Alaska',
+                'AZ' => 'Arizona',
+                'AR' => 'Arkansas',
+                'CA' => 'California',
+                'CO' => 'Colorado',
+                'CT' => 'Connecticut',
+                'DE' => 'Delaware',
+                'DC' => 'District of Columbia',
+                'FL' => 'Florida',
+                'GA' => 'Georgia',
+                'HI' => 'Hawaii',
+                'ID' => 'Idaho',
+                'IL' => 'Illinois',
+                'IN' => 'Indiana',
+                'IA' => 'Iowa',
+                'KS' => 'Kansas',
+                'KY' => 'Kentucky',
+                'LA' => 'Louisiana',
+                'ME' => 'Maine',
+                'MD' => 'Maryland',
+                'MA' => 'Massachusetts',
+                'MI' => 'Michigan',
+                'MN' => 'Minnesota',
+                'MS' => 'Mississippi',
+                'MO' => 'Missouri',
+                'MT' => 'Montana',
+                'NE' => 'Nebraska',
+                'NV' => 'Nevada',
+                'NH' => 'New Hampshire',
+                'NJ' => 'New Jersey',
+                'NM' => 'New Mexico',
+                'NY' => 'New York',
+                'NC' => 'North Carolina',
+                'ND' => 'North Dakota',
+                'OH' => 'Ohio',
+                'OK' => 'Oklahoma',
+                'OR' => 'Oregon',
+                'PA' => 'Pennsylvania',
+                'RI' => 'Rhode Island',
+                'SC' => 'South Carolina',
+                'SD' => 'South Dakota',
+                'TN' => 'Tennessee',
+                'TX' => 'Texas',
+                'UT' => 'Utah',
+                'VT' => 'Vermont',
+                'VA' => 'Virginia',
+                'WA' => 'Washington',
+                'WV' => 'West Virginia',
+                'WI' => 'Wisconsin',
+                'WY' => 'Wyoming',
+            ),
+        ));
+    }
+} 
